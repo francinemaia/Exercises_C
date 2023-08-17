d3e715a422a1a3ed5959f99b0a8d6a316783bf03
Fran <francinerodriguesmaia@gmail.com>
2023-08-18 00:32:15 +0100

    putchar

---
+++ ft_putchar.c
@@ -0,0 +1,14 @@
+/*Write a function that displays the character passed as a parameter.*/
+
+#include <unistd.h>
+
+void ft_putchar(char c)
+{
+    write(1, &c, 1);
+}
+
+/*int   main(void)
+{
+    ft_putchar('c');
+    return (0);
+}*/

fd51d80e561ff7e54db06ef84066a0304ab2b1d0
Fran <francinerodriguesmaia@gmail.com>
2023-08-18 23:31:39 +0100

    ft_strlen

---
+++ ft_strlen.c
@@ -0,0 +1,15 @@
+/*Write a function that counts and returns the number of characters in a string*/
+
+#include <stdio.h>
+
+int ft_strlen(char *str)
+{
+    int i;
+
+    i = 0;
+    while (str[i] != '\0')
+    {
+        i++;
+    }
+    return (i);
+}
